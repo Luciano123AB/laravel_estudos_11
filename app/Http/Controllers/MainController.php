d1c1de46bba21ef67282dc66c68270f0d48c61d7
Luciano Eduardo <luciano123ab@gmail.com>
2026-03-20 19:29:41 -0300

    007(Controller_AutorizacaoGates)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class MainController extends Controller
@@ -14,5 +15,21 @@ class MainController extends Controller
         $posts = Post::with("user")->get();
 
         return view("dashboard", ["posts" => $posts]);
+    }
+    public function createPost() {
+        //Gate:
+        if (Gate::denies("post.create")) {
+            abort(403, "Você não tem permissão para criar posts.");
+        }
+
+        echo "Create Post!";
+    }
+    public function deletePost() {
+        //Gate:
+        if (Gate::denies("post.delete")) {
+            abort(403, "Você não tem permissão para eliminar um post.");
+        }
+
+        echo "Delete Post!";
     }
 }
